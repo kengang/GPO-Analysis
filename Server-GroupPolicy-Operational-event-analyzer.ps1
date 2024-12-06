1ee1c3da7b78364390fd85933966d8eefbedfe89
kengang <36960009+kengang@users.noreply.github.com>
2024-12-05 22:30:37 -0500

    Add files via upload
    add computer policy analysis, run the script with computer or user option

--- Server-GroupPolicy-Operational-event-analyzer.ps1
+++ Server-GroupPolicy-Operational-event-analyzer.ps1
@@ -1,13 +1,9 @@
 ﻿##########################################################################
 #  Script to analyze Microsoft-Windows-GroupPolicy-Operational eventlog  #
-# It's only to analyze processing of User policy, and NOT Machine policy #
+# It's to analyze processing of User policy, or  Machine policy          #
 # It's only for testing purpose and not for production use               #
 ##########################################################################
-#                                                                        # 
-# The input file need to be in ANSI format                               #
-# GPPRef user logs need manually open the file in notepad and save using # 
-# ANSI encoding; script is meant to be run in Powershell_ISE             #
-########################################################################## 
+
 <#     
 Author: Ken Mei
 
@@ -28,7 +24,7 @@ from the use or distribution of the Sample Code.
  
 
 HOw-to:
-This script is meant to run from powershel_ISE
+
 This is to analaze GPP client-side extension processing base on eventlogs
 how to eport the eventlogs
 
@@ -40,20 +36,7 @@ https://learn.microsoft.com/en-us/troubleshoot/windows-server/group-policy/apply
 #>
 
 #Run directly on the server you want to invetigate
-
-$path = $Logname = "Microsoft-Windows-GroupPolicy/Operational"
-
-$max = 1000 # look at latest 1000 events only
-$activityList = @()
-$evtID4001 = "4001"
-$evtID5017 = "5017"
-$evtID4016 = "4016"
-$evtID5016 = "5016"
-$evtID5326 = "5326"
-$evtID8001 = "8001"
-$evtID5117 = "5117"
-$evtID5126 = "5126"
-
+$choice = Read-Host "Please enter COMPUTER OR USER to proceed"
 
 #Function to find Events
 Function find-events {
@@ -67,80 +50,281 @@ Function find-events {
   return $evt
 }
 
+Function get-user-policy-data{
+    param(
+      [string]$logname = "Microsoft-Windows-GroupPolicy/Operational",
+      [int]$maxEvt = 2000
+    )
 
-$activityIDs = Get-WinEvent -LogName $Logname  -MaxEvents $max |Where-Object {$_.id -match $evtID4001}| ForEach-Object {$_.ActivityId.Guid} | select -Unique
+    $activityList = @()
+    $evtID4001 = "4001"
+    $evtID5017 = "5017"
+    $evtID4016 = "4016"
+    $evtID5016 = "5016"
+    $evtID5326 = "5326"
+    $evtID8001 = "8001"
+    $evtID5117 = "5117"
+    $evtID5126 = "5126"
 
 
-$collsessions = @()
-foreach ($activitID in $activityIDs) {
+    $activityIDs = Get-WinEvent -LogName $Logname  -MaxEvents $maxEvt |Where-Object {$_.id -match $evtID4001}| ForEach-Object {$_.ActivityId.Guid} | select -Unique
+   
+    $ErrorSessions = @()
+    $collsessions = @()
+    foreach ($activitID in $activityIDs) {
 
-     $query = ("<QueryList><Query Id=""0"" Path=""Microsoft-Windows-GroupPolicy/Operational""><Select Path=""Microsoft-Windows-GroupPolicy/Operational"">*[System/Correlation/@ActivityID='{$activitID}']</Select></Query></QueryList>")
-     $events=Get-WinEvent -LogName $Logname -FilterXPath $query  -Oldest
-       
-     $evt4001 = find-events -array $events -evtID $evtID4001
-     $evt8001 = find-events -array $events -evtID $evtID8001
-     $evt4016 = find-events -array $events -evtID $evtID4016
-     $evt5016 = find-events -array $events -evtID $evtID5016
-     $evt5017 = find-events -array $events -evtID $evtID5017
-     $evt5117 = find-events -array $events -evtID $evtID5117
-     $evt5126 = find-events -array $events -evtID $evtID5126
+         $query = ("<QueryList><Query Id=""0"" Path=""$logname""><Select Path=""$logname"">*[System/Correlation/@ActivityID='{$activitID}']</Select></Query></QueryList>")
+         $events=Get-WinEvent -LogName $Logname -FilterXPath $query  -Oldest
+
+         if ($events.LevelDisplayName -contains "error") {
+            $eventerrors = $events | where-Object { $_.LevelDisplayName -eq "Error"}
+            # Create a new object
+             $errobj = New-Object PSObject -Property @{
+                            error_time = $events[0].TimeCreated
+                            activityID = $activitID
+            }
+            $ErrorSessions += $errobj
+     
+         } else{
+             $evt4001 = find-events -array $events -evtID $evtID4001
+             $evt8001 = find-events -array $events -evtID $evtID8001
+             $evt4016 = find-events -array $events -evtID $evtID4016
+             $evt5016 = find-events -array $events -evtID $evtID5016
+             $evt5017 = find-events -array $events -evtID $evtID5017
+             $evt5117 = find-events -array $events -evtID $evtID5117
+             $evt5126 = find-events -array $events -evtID $evtID5126
           
-     $evtxml4001 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml4001 = ([Xml]($evt4001.ToXml())).event
+             $evtxml4001 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml4001 = ([Xml]($evt4001.ToXml())).event
      
-     $evtxml8001 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml8001 = ([Xml]($evt8001.ToXml())).event
+             $evtxml8001 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml8001 = ([Xml]($evt8001.ToXml())).event
 
-     $evtxml5126 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml5126 = $evt5126 | ForEach-Object { ([xml]$_.toxml()).event}
+             $evtxml5126 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml5126 = $evt5126 | ForEach-Object { ([xml]$_.toxml()).event}
 
-     $evtxml5117 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml5117 = ([Xml]($evt5117.ToXml())).event
+             $evtxml5117 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml5117 = ([Xml]($evt5117.ToXml())).event
 
-     $evtxml5016 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml5016 = $evt5016 | ForEach-Object { ([xml]$_.toxml()).event}
+             $evtxml5016 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml5016 = $evt5016 | ForEach-Object { ([xml]$_.toxml()).event}
 
-     $evtxml4016 = New-Object -TypeName System.Xml.XmlDocument
-     $evtxml4016 = $evt4016 | ForEach-Object { ([xml]$_.toxml()).event}
+             $evtxml4016 = New-Object -TypeName System.Xml.XmlDocument
+             $evtxml4016 = $evt4016 | ForEach-Object { ([xml]$_.toxml()).event}
      
-     $collcesobj = @()
-     for($i=0; $i -lt $evtxml5016.count ; $i++) {
+             $collcesobj = @()
+             for($i=0; $i -lt $evtxml5016.count ; $i++) {
         
-         $CSE = $evtxml5016[$i].EventData.Data[2].'#text'
-         $CSE_duration = $evtxml5016[$i].EventData.Data[0].'#text'
-         $policy_in_CSE = ($evtxml4016[$i].EventData.Data[5].'#text').Split([Environment]::NewLine)
-        # Create a new object
-          $cseobj = New-Object PSObject -Property @{
-                    cseName = $CSE
-                    cseTotalTime = $CSE_duration
-                    csePolicies =  $policy_in_CSE
-                    }
+                 $CSE = $evtxml5016[$i].EventData.Data[2].'#text'
+                 $CSE_duration = $evtxml5016[$i].EventData.Data[0].'#text'
+                 $policy_in_CSE = ($evtxml4016[$i].EventData.Data[5].'#text').Split([Environment]::NewLine)
+                # Create a new object
+                  $cseobj = New-Object PSObject -Property @{
+                            cseName = $CSE
+                            cseTotalTime = $CSE_duration
+                            csePolicies =  $policy_in_CSE
+                            }
 
-          $collcesobj += $cseobj
-      }
+                  $collcesobj += $cseobj
+              }
     
       
-      # Create a new object
-      $sessionObj = New-Object PSObject -Property @{
-                    Username = $evtxml4001.EventData.Data[1].'#text'  
-                    pid = $evtxml4001.System.Execution.ProcessID
-                    tid = $evtxml4001.System.Execution.ThreadID
-                    SessionDuration = $evtxml5117.EventData.data[1].'#text'
-                    PolicyElaspedTimeInSeconds = $evtxml8001.EventData.data[0].'#text'
-                    GPODownloadTimeInMilliseconds = $evtxml5126.EventData.data[5].'#text'
-                    CSE = $collcesobj
-                    }                 
-     $collsessions += $sessionObj
+              # Create a new object
+              $sessionObj = New-Object PSObject -Property @{
+                            Username = $evtxml4001.EventData.Data[1].'#text'  
+                            pid = $evtxml4001.System.Execution.ProcessID
+                            tid = $evtxml4001.System.Execution.ThreadID
+                            sessionstart = $evt4001.TimeCreated
+                            sessionend = $evt5117.TimeCreated
+                            SessionDuration = $evtxml5117.EventData.data[1].'#text'
+                            PolicyElaspedTimeInSeconds = $evtxml8001.EventData.data[0].'#text'
+                            GPODownloadTimeInMilliseconds = $evtxml5126.EventData.data[5].'#text'
+                            CSE = $collcesobj
+                            }                 
+             $collsessions += $sessionObj
+
+         }
 
 }
 
-foreach ($evt in  $collsessions){
+    foreach ($evt in  $collsessions){
+         write-host "***************************************************************************"
+         write-host "****************************************************************************"
+         write-host "****************************************************************************"
+         write-host "This is session: " $evt.PID":"$evt.TID -ForegroundColor Magenta
+         write-host "session start: " $evt.Sessionstart
+         write-host "session end: " $evt.Sessionend
+         Write-host "->UserName" $evt.Username 
+         write-host "-->Total Duration for session in Second: "  ($evt.SessionDuration/1000)
+         Write-host "---->Total Time to download all policies in Second: " ($evt.GPODownloadTimeInMilliseconds/1000)
+         write-host "--------------------------------"
+         foreach ($cse in $evt.CSE) {
+
+             write-host "------> Client-Side Extension Name: " $cse.cseName -ForegroundColor Green
+             write-host "-------> Total Duration in milliseconds: " $cse.cseTotalTime
+             write-host "----------->Policy being process: " 
+             foreach ($policy in $cse.csePolicies) {
+                write-host "               " $policy -ForegroundColor yellow
+            
+             }
+
+          }
+}
+    if ($ErrorSessions -ne $null) {
+       write-host "******************************" -ForegroundColor Red
+       write-host "******************************" -ForegroundColor Red
+
+       foreach ($session in $ErrorSessions){  
+       write-host "Below session has error, please examine manual by creating a custom view in eventlog"
+       write-host "  date/time: "$session.error_time
+       Write-Host "  session activity id: " $session.activityID
+       write-host "  xml filter: " ("<QueryList><Query Id=""0"" Path=""file://$Logname""><Select Path=""file://$Logname"">*[System/Correlation/@ActivityID='{$session}']</Select></Query></QueryList>") -ForegroundColor Magenta
+    }
+}
+}
+
+Function get-computer-policy-data {
+    param(
+       [string]$Logname="Microsoft-Windows-GroupPolicy/Operational",
+       [int]$maxEvt = 2000
+    )
+    
+    $activityList = @()
+    $evtID4000 = "4000" #start session
+    $evtID5017 = "5017"
+    $evtID4016 = "4016" #start cse
+    $evtID5016 = "5016" #complete cse
+    $evtID7016 = "7016" #complete cse with error
+    $evtID5326 = "5326" #discover policy
+    $evtID5257 = "5257" #download plicies
+    $evtID8000 = "8000" #complete boot policy processing
+    $evtID5117 = "5117" #complete the whole session
+    $evtID5126 = "5126" #download plicies
+    $evtID4018 = "4018" #startup script
+    $evtID5018 = "5018" #end of Startup SCript
+
+       
+    $activityIDs = Get-WinEvent -LogName $Logname  -MaxEvents $maxEvt |Where-Object {$_.id -match $evtID4000}| ForEach-Object {$_.ActivityId.Guid} | select -Unique
+
+    $ErrorSessions = @()
+    $collsessions = @()
+
+                                                                                                                                                                                                                                                                                                                                                    if ($activityIDs -ne $null){
+    foreach ($activitID in $activityIDs) {
+
+        $query = ("<QueryList><Query Id=""0"" Path=""$Logname""><Select Path=""$Logname"">*[System/Correlation/@ActivityID='{$activitID}']</Select></Query></QueryList>")
+        $events=Get-WinEvent -LogName $Logname -FilterXPath $query  -Oldest
+     
+        if ($events.LevelDisplayName -contains "error") {
+            $eventerrors = $events | where-Object { $_.LevelDisplayName -eq "Error"}
+             # Create a new object
+             $errobj = New-Object PSObject -Property @{
+                            error_time = $events[0].TimeCreated
+                            activityID = $activitID
+            }
+            $ErrorSessions += $errobj
+     
+         } else{
+
+
+         if ($events.id -contains "$evtID4018") {
+        
+            $starucript = "true"
+         } else {
+            $starucript = "false"
+         }
+     
+         write-host $activityID
+    
+         $evt4000 = find-events -array $events -evtID $evtID4000
+         $evt8000 = find-events -array $events -evtID $evtID8000
+         $evt4016 = find-events -array $events -evtID $evtID4016
+         $evt5016 = find-events -array $events -evtID $evtID5016
+         $evt5017 = find-events -array $events -evtID $evtID5017
+         $evt5117 = find-events -array $events -evtID $evtID5117
+         $evt5126 = find-events -array $events -evtID $evtID5126
+         $evt4018 = find-events -array $events -evtID $evtID4018
+         $evt5018 = find-events -array $events -evtID $evtID5018
+          
+         $evtxml4000 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml4000 = ([Xml]($evt4000.ToXml())).event
+     
+         $evtxml8000 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml8000 = ([Xml]($evt8000.ToXml())).event
+
+         $evtxml5126 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml5126 = $evt5126 | ForEach-Object { ([xml]$_.toxml()).event}
+
+         $evtxml5117 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml5117 = ([Xml]($evt5117.ToXml())).event
+
+     
+         $starupscript_duration = $null
+         if ($evt5018 -ne $null) {
+                $evtxml4018 = New-Object -TypeName System.Xml.XmlDocument
+                $evtxml4018 = $evt4018 | ForEach-Object { ([xml]$_.toxml()).event}
+                $evtxml5018 = New-Object -TypeName System.Xml.XmlDocument
+                $evtxml5018 = ([Xml]($evt5018.ToXml())).event
+                $starupscript_duration = $evtxml5018.EventData.data[0].'#text'
+         } 
+
+         $evtxml5016 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml5016 = $evt5016 | ForEach-Object { ([xml]$_.toxml()).event}
+
+         $evtxml4016 = New-Object -TypeName System.Xml.XmlDocument
+         $evtxml4016 = $evt4016 | ForEach-Object { ([xml]$_.toxml()).event}
+     
+         $collcesobj = @()
+         for($i=0; $i -lt $evtxml5016.count ; $i++) {
+        
+             $CSE = $evtxml5016[$i].EventData.Data[2].'#text'
+             $CSE_duration = $evtxml5016[$i].EventData.Data[0].'#text'
+             $policy_in_CSE = ($evtxml4016[$i].EventData.Data[5].'#text').Split([Environment]::NewLine)
+            # Create a new object
+              $cseobj = New-Object PSObject -Property @{
+                        cseName = $CSE
+                        cseTotalTime = $CSE_duration
+                        csePolicies =  $policy_in_CSE
+                        }
+
+              $collcesobj += $cseobj
+          }
+    
+      
+          # Create a new object
+          $sessionObj = New-Object PSObject -Property @{
+                        Username = $evtxml4000.EventData.Data[1].'#text'  
+                        pid = $evtxml4000.System.Execution.ProcessID
+                        tid = $evtxml4000.System.Execution.ThreadID
+                        sessionstart = $evt4000.TimeCreated
+                        sessionend = $evt5117.TimeCreated
+                        SessionDuration = $evtxml5117.EventData.data[1].'#text'
+                        PolicyElaspedTimeInSeconds = $evtxml8000.EventData.data[0].'#text'
+                        GPODownloadTimeInMilliseconds = $evtxml5126.EventData.data[5].'#text'
+                        ScriptElaspedTimeInSeconds = $starupscript_duration
+                        CSE = $collcesobj
+                        }                 
+         $collsessions += $sessionObj
+
+    }
+    }
+    } else{  write-host "No Activity found"
+        }
+
+
+    foreach ($evt in  $collsessions){
      write-host "***************************************************************************"
      write-host "****************************************************************************"
      write-host "****************************************************************************"
      write-host "This is session: " $evt.PID":"$evt.TID -ForegroundColor Magenta
+     write-host "session start: " $evt.Sessionstart
+     write-host "session end: " $evt.Sessionend
      Write-host "->UserName" $evt.Username 
-     write-host "-->Total Duration for session in Second: "  ($evt.SessionDuration/1000)
+     write-host "-->Total Duration for session in Second: "  ($evt.SessionDuration /1000)
+     write-host "--------->Boot policy processing time in second: " ($evt.PolicyElaspedTimeInSeconds)
+     if ($starupscript_duration -ne $null) {   
+     write-host "--------->Startup Script processing time in second: " ($evt.ScriptElaspedTimeInSeconds)
+     }
      Write-host "---->Total Time to download all policies in Second: " ($evt.GPODownloadTimeInMilliseconds/1000)
      write-host "--------------------------------"
      foreach ($cse in $evt.CSE) {
@@ -154,4 +338,24 @@ foreach ($evt in  $collsessions){
          }
 
       }
+}
+
+    if ($ErrorSessions -ne $null) {
+       write-host "******************************" -ForegroundColor Red
+       write-host "******************************" -ForegroundColor Red
+
+       foreach ($session in $ErrorSessions){  
+           write-host "Below session has error, please examine manual by creating a custom view in eventlog"
+           write-host "  date/time: "$session.error_time
+           Write-Host "  session activity id: " $session.activityID
+           write-host "xml filter: " ("<QueryList><Query Id=""0"" Path=""file://$Logname""><Select Path=""file://$Logname"">*[System/Correlation/@ActivityID='{$session}']</Select></Query></QueryList>") -ForegroundColor Magenta
+        }
+    }
+}
+
+switch ($choice) {
+  "computer" {get-computer-policy-data}
+  "user" {get-user-policy-data}
+  default {get-user-policy-data}
+    
 }
